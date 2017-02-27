1bde72a0201c5e69419422cc948716f6808a465b
snaprud <thomas@kodeks.no>
2017-02-27 13:39:41 +0100

    WooCommerce
    Added support for WooCommerce

--- functions.php
+++ functions.php
@@ -200,3 +200,24 @@ add_filter('wp_nav_menu_items','add_search_to_wp_menu',10,2);
 /* Remove edit-button */
 
 add_filter( 'edit_post_link', '__return_false' );
+
+/* WooCommerce */
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<div id="primary" class="content-area row small-12 medium-12">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</div>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
